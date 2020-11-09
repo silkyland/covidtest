0654b4abcd2edccfb8457904aceb5f48db944aef
Bundit Nuntates <silkyland@gmail.com>
2020-11-09 23:53:52 +0700

    buxfix

--- src/store/actions/covid.ts
+++ src/store/actions/covid.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Dispatch } from "react";
 import { Action } from "redux";
 
 export const SET_TODO = "SET_TODO";
@@ -11,35 +12,37 @@ export enum CovidAction {
   DONE_DELETE = "DONE_DELETE",
 }
 
-export const getTodo = () => async (dispatch) => {
+const config: any = {};
+
+export const getTodo = () => async (dispatch: Dispatch<any>) => {
   let todos = await axios.get(config.server.api + "/todos");
   dispatch(setTodos(todos.data));
 };
 
-export const addTodo = (message) => async (dispatch) => {
+export const addTodo = (message: string) => async (dispatch: Dispatch<any>) => {
   let todos = await axios.post(config.server.api + "/todos", {
     name: message,
   });
   dispatch(setTodo(todos.data));
 };
 
-export const deleteTodo = (id) => async (dispatch) => {
+export const deleteTodo = (id: string) => async (dispatch: Dispatch<any>) => {
   let todo = await axios.delete(config.server.api + "/todos/" + id);
   let todos = await axios.get(config.server.api + "/todos");
   dispatch(setTodos(todos.data));
 };
 
-const doneDelete = (id) => ({
+const doneDelete = (id: string) => ({
   type: DONE_DELETE,
   payload: id,
 });
 
-const setTodo = (data) => ({
+const setTodo = (data: CovidAction) => ({
   type: SET_TODO,
   payload: data,
 });
 
-const setTodos = (data) => ({
+const setTodos = (data: CovidAction) => ({
   type: SET_TODOS,
   payload: data,
 });
